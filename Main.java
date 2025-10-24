ae554aec399b23d6911fa46d750ce8fead2e3440
SimeonC2026 <simeon.chew2026@gbwl.org>
2025-10-24 12:55:53 +0000

    Part B for project

--- Main.java
+++ Main.java
@@ -1,6 +1,22 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      System.out.println("Simeon's Digital RECEIPT");
+      
+      // CODE FOR PART B STARTS HERE
+      String highSchoolName = "Birch Wathen Lenox School";
+      double drinkPrice = 1.50;
+
+      // RECEIPT PRINTOUT SECTION
+     System.out.println("**************************************");
+     System.out.println("*                                    *");
+     System.out.println("*     " + highSchoolName + " Snack Bar          *");
+     System.out.println("*                                    *");
+     System.out.println("*     Drink ..........$" + drinkPrice + "        *");                      
+     System.out.println("*     Candy ..........$1.25          *");     
+     System.out.println("*     Hot Dog ........$2.75          *");     
+     System.out.println("*     Hamburger ......$3.50          *");     
+     System.out.println("*                                    *");    
+     System.out.println("**************************************");
    }
 }
