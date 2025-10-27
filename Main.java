4f6f587e800972df5a211ceb2ee26c0c0389f823
SimeonC2026 <simeon.chew2026@gbwl.org>
2025-10-27 13:40:20 +0000

    Added some more variables

--- Main.java
+++ Main.java
@@ -6,16 +6,27 @@ public class Main {
       // CODE FOR PART B STARTS HERE
       String highSchoolName = "Birch Wathen Lenox School";
       double drinkPrice = 1.50;
-
+      double candyPrice = 1.25;
+      double HotDogPrice = 2.75;
+      double HamburgerPrice = 3.50;
+      int OrderNumber = 4;
+      int drinkOrdered = 7;
+      int candyOrdered = 3;
+      int hotdogOrdered = 5;
+      int hamburgerOrdered = 2;
+      double taxRate = 0.08;
+      double subtotal = drinkPrice * quantityDrink + 
+      double totalTax = subtotal * taxRate;
+      double total = subtotal + totalTax;
       // RECEIPT PRINTOUT SECTION
      System.out.println("**************************************");
      System.out.println("*                                    *");
      System.out.println("*     " + highSchoolName + " Snack Bar          *");
      System.out.println("*                                    *");
      System.out.println("*     Drink ..........$" + drinkPrice + "        *");                      
-     System.out.println("*     Candy ..........$1.25          *");     
-     System.out.println("*     Hot Dog ........$2.75          *");     
-     System.out.println("*     Hamburger ......$3.50          *");     
+     System.out.println("*     Candy ..........$" + candyPrice + "         *");     
+     System.out.println("*     Hot Dog ........$" + HotDogPrice + "        *");     
+     System.out.println("*     Hamburger ......$" + HamburgerPrice + "        *");     
      System.out.println("*                                    *");    
      System.out.println("**************************************");
    }
